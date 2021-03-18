9ea960018c050fa92852e5c0c8eef28ec438f571
Deepak Rawat <52197650+DeepakRawat24@users.noreply.github.com>
2021-03-18 21:34:38 +0530

    BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,50 @@
+// binary search firnt will be in sorted order
+
+#include <iostream>
+using namespace std;
+
+int binarynsearch(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n;
+
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cout << "enter size of array: \n";
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int key;
+    cout << "enter key: ";
+    cin >> key;
+
+    cout << binarynsearch(arr, n, key);
+
+    return 0;
+}
