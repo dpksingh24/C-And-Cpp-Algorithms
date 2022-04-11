0c02816d782a78c8152ae85031c4b46bc1fd7d13
Deepak Rawat <52197650+DeepakRawat24@users.noreply.github.com>
2022-04-11 09:24:01 +0530

    Update ReverseString.cpp

--- stringProblems/ReverseString.cpp
+++ stringProblems/ReverseString.cpp
@@ -3,20 +3,47 @@ using namespace std;
 
 int main()
 {
-  	string str = "deepak";
-	int len = str.length();
-    int i = 0;
-    int j = len - 1;
-    int temp = 0;
-    while (i < j)
-    {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
+    string str = "deepak";
+    int len = str.length();
 
-        i++;
-        j--;
+    char a = 0;
+    char b = len - 1;
+    char temp = 0;
+    while (a < b)
+    {
+        temp = str[a];
+        str[a] = str[b];
+        str[b] = temp;
+
+        a++;
+        b--;
     }
     cout << str;
+
+    return 0;
+}
+
+
+/* 2nd method */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str = "deepak";
+
+    int len = str.length();
+    int n = len - 1;
+    for (int i = 0; i < len / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[n];
+        str[n] = temp;
+        n = n - 1;
+    }
+
+    cout << str;
+
     return 0;
 }
