58c9c9dbde304b8d35719e9c25e6804cb303757a
Deepak Rawat <52197650+DeepakRawat24@users.noreply.github.com>
2024-06-12 16:35:11 +0530

    dub new method

--- ror/findDup.rb
+++ ror/findDup.rb
@@ -7,3 +7,25 @@ def find_duplicates(array)
 end
 
 p find_duplicates([1, 2, 4,4 ])
+
+
+
+# method 2
+require 'set'
+
+def find_duplicates(array)
+  seen = Set.new
+  duplicates = Set.new
+
+  array.each do |element|
+    if seen.include?(element)
+      duplicates.add(element)
+    else
+      seen.add(element)
+    end
+  end
+
+  duplicates.to_a
+end
+
+p find_duplicates([1, 2, 4, 4]) # => [4]
